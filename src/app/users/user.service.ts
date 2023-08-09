97d67ba6f3f920f8d4a833b98e7e23c464b78e92
Sanjay Garg <sgarg5858@gmail.com>
2023-08-09 19:32:54 +0530

    caching the observable

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from './user.model';
-import { catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, share } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,20 @@ export class UserService {
 
   constructor(private httpClient:HttpClient) { }
 
+  users$:Observable<User[]>|undefined;
+
   getUsers()
   {
-    return this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users').pipe(
-      catchError(()=>of([]))
-    );
+    if(!this.users$)
+    {
+      //Here we are caching the observable, but this will not work as 
+      // each time subscribe is called on this observable it will make http call
+      //as observables are unicast
+      this.users$= this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users').pipe(
+        catchError(()=>of([]))
+      );
+    }
+    return this.users$;
   }
   getUser(id:number)
   {
