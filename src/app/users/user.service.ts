b40d4edfd30a5276dc7b78df59d46a1923e954c2
Sanjay Garg <sgarg5858@gmail.com>
2023-08-09 19:33:37 +0530

    share will also not work

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -20,6 +20,7 @@ export class UserService {
       // each time subscribe is called on this observable it will make http call
       //as observables are unicast
       this.users$= this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users').pipe(
+        share(),
         catchError(()=>of([]))
       );
     }
